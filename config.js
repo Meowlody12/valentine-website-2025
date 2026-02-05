446c34230daa3abbda7460b75278346d7a76ad20
Meowlody12 <melodyyx@outlook.com>
2026-02-05 12:53:05 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Jade",
+    valentineName: "Shen Jiaying",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
@@ -22,16 +22,16 @@ const CONFIG = {
     // Customize each question and its possible responses
     questions: {
         first: {
-            text: "Do you like me?",                                    // First interaction
-            yesBtn: "Yes",                                             // Text for "Yes" button
-            noBtn: "No",                                               // Text for "No" button
-            secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
+            text: "Day or Night",                                    // First interaction
+            yesBtn: "Day",                                             // Text for "Yes" button
+            noBtn: "Night",                                               // Text for "No" button
+            secretAnswer: "no right answer hehe"           // Secret hover message
         },
-        second: {
+       /* second: {
             text: "How much do you love me?",                          // For the love meter
             startText: "This much!",                                   // Text before the percentage
             nextBtn: "Next ❤️"                                         // Text for the next button
-        },
+        },*/
         third: {
             text: "Will you be my Valentine on February 14th, 2025? 🌹", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
@@ -41,11 +41,12 @@ const CONFIG = {
 
     // Love meter messages
     // They show up depending on how far they slide the meter
+    /*
     loveMessages: {
         extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
         high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
         normal: "And beyond! 🥰"                           // Shows when they go past 100%
-    },
+    },*/
 
     // Messages that appear after they say "Yes!"
     celebration: {
